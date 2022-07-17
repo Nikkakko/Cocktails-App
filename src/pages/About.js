ee3987e4a0cf55c0b24208edded328d293eac10d
Nikkakko <nikolozkopadze@gmail.com>
2022-07-17 09:30:38 +0400

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'> about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea odit eius
+        nostrum necessitatibus enim repudiandae ab voluptas nesciunt quasi
+        consectetur iure debitis quas doloribus magni alias aliquid nulla
+        molestiae atque, maiores a quos natus consequuntur? Eos ut corporis illo
+        maxime officiis soluta voluptate, vero omnis vel magni laborum sapiente
+        fugit!
+      </p>
+    </section>
+  );
+};
+
+export default About;
